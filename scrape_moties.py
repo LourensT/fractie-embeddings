fc1ef62c285bd563d22525824b8c41e909ea8676
lourenst <touwenlourens@gmail.com>
2023-10-16 22:59:51 +0200

    find motions from Zaak with daterange filtering

--- scrape_moties.py
+++ scrape_moties.py
@@ -1,13 +1,13 @@
 # %%
 import tkapi
 from tkapi.stemming import Stemming
-import json
+from tkapi.zaak import ZaakMotie
 
-from pprint import pprint
+import json
 import datetime
+from pprint import pprint
 
 from requests.exceptions import ConnectionError
-
 
 # %%
 def get_stemmingen_per_fractie(fractie, max_items = 100):
@@ -22,7 +22,7 @@ def get_decisions_per_fractie_from_besluit(besluit):
     response['resultaat'] = besluit.tekst
     response['onderwerp'] = besluit.agendapunt.onderwerp
     response['besluit_id'] = besluit.id
-    response['datum'] = besluit.gewijzigd_op.strftime("%d-%m-%Y %H:%M:%S")
+    response['datum'] = besluit.gewijzigd_op.strftime("%Y-%m-%d %H:%M:%S")
     
     stemgedrag = {}
     stemmingen = besluit.stemmingen
@@ -34,41 +34,48 @@ def get_decisions_per_fractie_from_besluit(besluit):
     # pprint(response)
     return response
 
-def dump_moties():
-    # get any active fractie
-    all_fracties = api.get_fracties(max_items=100)
-    fracties_met_zetels = []
-    for fractie in all_fracties:
-        if not fractie.zetels_aantal is None and fractie.zetels_aantal  > 0:
-            fracties_met_zetels.append(fractie)
-
-    # select an active fractie, VVD is biggest and coalition, so most likely to have decisions
-    for fractie in fracties_met_zetels:
-        if fractie.naam == "Volkspartij voor Vrijheid en Democratie":
-            vvd = fractie
-            break
-
-    print(f"using as index the party {vvd.naam}")
-
-    # scrape backwards in time to get all decisions
-    stemmingen = get_stemmingen_per_fractie(vvd, max_items = 10000)
-    print(f"fetching {len(stemmingen)} decisions")
-    for i, stemming in enumerate(stemmingen):
-        if i % 25 == 0:
-            print(f"fetching decision {i} of {len(stemmingen)}")
-        try:
-            besluit = stemming.besluit
-            res = get_decisions_per_fractie_from_besluit(besluit)
-        except ConnectionError:
-            print("ConnectionError, skipping")
-            continue
-
-        fp = f"./data/{res['datum'][:10]}_{res['onderwerp']}.json"
-        with open(fp, "w") as f:
-            json.dump(res, f)
+def save_res(res):
+    subject = res['onderwerp']
+    subject = subject.replace("/", "")
+    subject = subject.replace("\\", "")
+    if len(subject) > 200:
+        subject = subject[:200]
     
+    fp = f"{OUTPUT_FP}{res['datum'][:10]}_{subject}.json"
+
+    with open(fp, "w") as f:
+        json.dump(res, f)
+
+def get_moties():
+    filter = ZaakMotie.create_filter()
+    filter.filter_soort('Motie')
+    filter.filter_date_range(START, END)
+    zaken = api.get_zaken(filter=filter, max_items=1000)
+
+    print("Starting...")
+    i = 0
+    errors = 0
+    for zaak in zaken: 
+        besluiten = zaak.besluiten
+        for besluit in besluiten:
+            i+=1
+            if i%25 == 0:
+                print(f"Handled {i}")
+
+            try:
+                res = get_decisions_per_fractie_from_besluit(besluit)
+                save_res(res)
+            except ConnectionError:
+                errors += 1
+                print("Connection Error")
+        
+        if errors > 50:
+            break
+# %%
 if __name__ == "__main__":
     api = tkapi.TKApi()
-    OUTPUT_FP = "./data/"
-    dump_moties()
-# %%
+    OUTPUT_FP = "./data2/"
+    START = datetime.datetime(2022, 2, 11)
+    END = datetime.datetime(2022, 9, 21)
+
+    get_moties()
